63a3b3adfa2410a2550da87415141a35fdc8fb8f
tod619 <trevorodwyer619@hotmail.com>
2021-03-19 15:12:41 +0000

    change txt in toggle btn on click in script.js

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 
-const time = document.querySelector('.time')
-const date = document.querySelector('.date')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 const toggle = document.querySelector('.toggle')
 const html = document.querySelector("html")
 
-toggle.addEventListener("click", ()=> {
+toggle.addEventListener("click", (e)=> {
     html.classList.toggle('dark')
+
+    if(html.classList.contains("dark")) {
+        e.target.innerHTML = "Light mode"
+    }else {
+        e.target.innerHTML = "Dark mode"
+    }
 })
 
